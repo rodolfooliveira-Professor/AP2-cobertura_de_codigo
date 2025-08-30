278bffc36207d74ee9254385dc9ccaa1e05e3918
rodolfooliveira-Professor <rodolfooliveira@id.uff.br>
2025-08-30 13:35:49 -0300

    Update order.test.js

--- __tests__/order.test.js
+++ __tests__/order.test.js
@@ -2,3 +2,8 @@
 * Esse trabalho é do grupo G#id
 */
 const { Order, Item } = require("../src/order");
+describe("Testes iniciais - placeholder", () => {
+  test("teste mínimo para validar setup", () => {
+    expect(true).toBe(true);
+  });
+});
